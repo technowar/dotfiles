3abbacab2553e31b6b0650cfa03fa1ee4d154e4f
Elizar Pepino <jupenz@gmail.com>
2019-06-01 08:04:39 +0800

    that's a wrap

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # ~/.*
 
-Updated and decluttered dotfiles with some additional setup scripts.
+Dotfiles et al
 
 &nbsp;
 
 
 	├── README.md
-	├── plugins.txt     // add your custom vim plugins here
+	├── images/         // screenshots
+	├── plugins.txt     // add your vim plugins here
 	├── setup.sh        // pre migration script
 	└── start.sh        // dotfiles migration
 
@@ -15,34 +16,72 @@ Updated and decluttered dotfiles with some additional setup scripts.
 ## Requirements
 
 - **`ruby`**
-
 - **`rake`**
-
+- **`node.js / yarn`**
+- **`go`** 1.12 or higher
 - **`git`**
-
-- **`vim/neovim`** - Install either of the two.
+- **`vim/neovim`** install either of the two.
 
 
 &nbsp;
 
-## Setup
+## Get Started
 
-> **Note:** 
-> 
-> Before doing the steps below, make sure to edit the **`.extra`** file first.
+> #### [NOTE] Before doing the steps below, make sure to edit the **`.extra`** file first.
+
+&nbsp;
 
 
 1. Run **`./setup.sh`** from your terminal window. This command could take a while. So get yourself a cup of coffee or a byte to eat.
+	
+	
+2. Run **`./start.sh`** to sync the dotfiles to your home dir. Once done, then you're good to go.
 
-	This will check for dependencies and install them if possible. And the following
-	tools and lib will also be installed: [`z.sh`](https://github.com/rupa/z/), [`janus`](https://github.com/carlhuda/janus)
-	
-	
-2. Next is **`./start.sh`**.
-
-	This will sync dotfiles to your home directory
-	
 	
 &nbsp;
 
-> つづく ( more to come... )
+## Addendum
+
+### What's in the box
+
+
+**Vim/NeoVim Stuff**
+
+- **[janus](https://github.com/carlhuda/janus)** - Plugin distribution awesomeness
+
+- **[coc.nvim](https://github.com/neoclide/coc.nvim/)** - Make your vim / neovim fly!
+
+	Btw. It's super easy to install plug-ins with COC. For instance. If you want node/js auto completion and code hinting. Just do `:CocInstall <plugin name>` inside your vim.
+
+	LANGUAGE SERVERS INSTALLED
+	
+	- **gopls**
+	- **flow**
+	
+	> will add more LS later
+
+&nbsp;
+
+**Bitchin' Prompt**
+
+![prompt](./images/term.png)
+
+&nbsp;
+
+### Custom configs and settings
+
+I've included an easy way to add additional exports and vimrc config.
+
+**~/.exports.after**
+
+To add your own set of exports without touching the `.exports` file, just create a
+`.exports.after` file in your home directory. The `.exports` file will actually check if this file exists and source it.
+
+
+**~/.vimrc.extra**
+
+You can create your own additional `.vimrc` settings by creating a `.vimrc.extra` file in your home dir. This will prevent you from touching the default `.vimrc` and `.vimrc.after` file.
+
+&nbsp;
+
+> つづく ( more later... )
--- setup.sh
+++ setup.sh
@@ -2,20 +2,20 @@
 
 # check vim, git, ruby and rake first
 if [[ ! -x $(which git) ]]; then
-	echo Error: git currently not installed
-	exit 1
+  echo Error: git currently not installed
+  exit 1
 fi
 if [[ ! -x $(which ruby) ]]; then
-	echo Error: ruby not installed
-	exit 1
+  echo Error: ruby not installed
+  exit 1
 fi
 if [[ ! -x $(which rake) ]]; then
-	echo Error: rake not installed
-	exit 1
+  echo Error: rake not installed
+  exit 1
 fi
 if [[ ! -x $(which nvim) ]] || [[ ! -x $(which vim) ]]; then
-	echo Error: rake not installed
-	exit 1
+  echo Error: rake not installed
+  exit 1
 fi
 
 # install z.sh
@@ -40,7 +40,67 @@ mkdir -p ~/.janus
 cd ~/.janus
 for repo in `cat "$dotfiles_folder"/plugins.txt`
 do
-	git clone $repo
+  git clone $repo
 done
 echo '  -> plugins installed'
 cd $dotfiles_folder
+
+# COC.nvim setup/install
+if [[ -x ~/.janus/coc.nvim/install.sh ]]; then
+  echo '  -> running coc.nvim start.sh script'
+  cd ~/.janus/coc.nvim/
+	if [[ ! -x $(which yarn) ]]; then
+		curl --compressed -o- -L https://yarnpkg.com/install.sh | bash
+	fi
+	yarn install --frozen-lockfile
+  echo '  -> coc.nvim done'
+	if [[ ! -f "$dotfiles_folder"/coc-settings.json ]]; then
+		touch "$dotfiles_folder"/coc-settings.json
+	fi
+  echo '{' > "$dotfiles_folder"/coc-settings.json
+  echo '  "languageserver": {' >> "$dotfiles_folder"/coc-settings.json
+
+  # Gopls for COC
+  if [[ -x $(which go) ]] && [[ $(go version | grep 1.12) ]]; then
+    echo '  -> setting up gopls for coc'
+		go get -u golang.org/x/tools/cmd/gopls
+		if [[ -x $(which gopls) ]]; then
+			echo '  -> gopls installed'
+			echo '     "golang": {' >> "$dotfiles_folder"/coc-settings.json
+			echo '       "command": "gopls",' >> "$dotfiles_folder"/coc-settings.json
+			echo '       "args": [],' >> "$dotfiles_folder"/coc-settings.json
+			echo '       "rootPatterns": ["go.mod", ".vim/", ".git/", ".hg/"],' >> "$dotfiles_folder"/coc-settings.json
+			echo '       "filetypes": ["go"]' >> "$dotfiles_folder"/coc-settings.json
+			echo -n '     }' >> "$dotfiles_folder"/coc-settings.json
+		fi
+		rm -rf /tmp/tools
+    cd $godoc_files
+  fi
+
+	# Flow for COC (javascript completion shit)
+	if [[ -x $(which npm) ]]; then
+		echo ' -> setting up flow for coc'
+		npm install -g flow-bin
+		if [[ -x $(which flow)  ]]; then
+			echo ' -> flow installed'
+			echo ',' >> "$dotfiles_folder"/coc-settings.json
+			echo '     "flow": {' >> "$dotfiles_folder"/coc-settings.json
+			echo '       "command": "flow",' >> "$dotfiles_folder"/coc-settings.json
+			echo '       "args": ["lsp"],' >> "$dotfiles_folder"/coc-settings.json
+			echo '       "initializationOptions": {},' >> "$dotfiles_folder"/coc-settings.json
+			echo '       "settings": {},' >> "$dotfiles_folder"/coc-settings.json
+			echo '       "requireRootPattern": true,' >> "$dotfiles_folder"/coc-settings.json
+			echo '       "rootPatterns": [".flowconfig"],' >> "$dotfiles_folder"/coc-settings.json
+			echo '       "filetypes": ["javascript", "javascriptreact"]' >> "$dotfiles_folder"/coc-settings.json
+			echo -n '     }' >> "$dotfiles_folder"/coc-settings.json
+		fi
+	fi
+
+  echo '' >> "$dotfiles_folder"/coc-settings.json
+  echo '  }' >> "$dotfiles_folder"/coc-settings.json
+  echo '}' >> "$dotfiles_folder"/coc-settings.json
+
+	echo ' -> moving config file to .vim folder'
+	cat "$dotfiles_folder"/coc-settings.json > ~/.vim/coc-settings.json
+	echo ' -> done'
+fi
