19e82d91843ecc5402a3b33d44fba9b4ec404795
Elizar Pepino <jupenz@gmail.com>
2019-06-02 15:22:37 +0800

    fix indention on start.sh

--- start.sh
+++ start.sh
@@ -2,18 +2,18 @@
 cd "$(dirname "${BASH_SOURCE}")"
 
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "start.sh" \
+  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "start.sh" \
     --exclude "setup.sh" --exclude "fonts" --exclude "vim-plugins.txt" \
-		--exclude "README.txt" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
-	source ~/.bash_profile
+    --exclude "README.txt" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
+  source ~/.bash_profile
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt
+  doIt
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
-	fi
+  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    doIt
+  fi
 fi
 unset doIt
