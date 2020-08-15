80f26335d8b85d05f34283dcdbecde421d9e466e
Johmel Rey Pintor <jahmgwapo@skolpad.org>
2020-08-15 18:36:14 +0800

    Update coc.nvim installation

--- start.sh
+++ start.sh
@@ -53,64 +53,62 @@ function setup() {
   cd $dotfiles_folder
 
   # COC.nvim setup/install
-  if [[ -x ~/.janus/coc.nvim/install.sh ]]; then
-    echo '  -> running coc.nvim start.sh script'
-    cd ~/.janus/coc.nvim/
-    if [[ ! -x $(which yarn) ]]; then
-      npm install -g yarn
-    fi
-    yarn install --frozen-lockfile
-    echo '  -> coc.nvim done'
-    if [[ ! -f "$dotfiles_folder"/coc-settings.json ]]; then
-      touch "$dotfiles_folder"/coc-settings.json
-    fi
-    echo '{' > "$dotfiles_folder"/coc-settings.json
-    echo '  "languageserver": {' >> "$dotfiles_folder"/coc-settings.json
-
-    # Gopls for COC
-    if [[ -x $(which go) ]] && [[ $(go version | grep 1.12) ]]; then
-      echo '  -> setting up gopls for coc'
-      go get -u golang.org/x/tools/cmd/gopls
-      if [[ -x $(which gopls) ]]; then
-        echo '  -> gopls installed'
-        echo '     "golang": {' >> "$dotfiles_folder"/coc-settings.json
-        echo '       "command": "gopls",' >> "$dotfiles_folder"/coc-settings.json
-        echo '       "args": [],' >> "$dotfiles_folder"/coc-settings.json
-        echo '       "rootPatterns": ["go.mod", ".vim/", ".git/", ".hg/"],' >> "$dotfiles_folder"/coc-settings.json
-        echo '       "filetypes": ["go"]' >> "$dotfiles_folder"/coc-settings.json
-        echo -n '     }' >> "$dotfiles_folder"/coc-settings.json
-      fi
-      rm -rf /tmp/tools
-      cd $godoc_files
-    fi
-
-    # Flow for COC (javascript completion shit)
-    if [[ -x $(which npm) ]]; then
-      echo ' -> setting up flow for coc'
-      npm install -g flow-bin
-      if [[ -x $(which flow)  ]]; then
-        echo ' -> flow installed'
-        echo ',' >> "$dotfiles_folder"/coc-settings.json
-        echo '     "flow": {' >> "$dotfiles_folder"/coc-settings.json
-        echo '       "command": "flow",' >> "$dotfiles_folder"/coc-settings.json
-        echo '       "args": ["lsp"],' >> "$dotfiles_folder"/coc-settings.json
-        echo '       "initializationOptions": {},' >> "$dotfiles_folder"/coc-settings.json
-        echo '       "settings": {},' >> "$dotfiles_folder"/coc-settings.json
-        echo '       "requireRootPattern": true,' >> "$dotfiles_folder"/coc-settings.json
-        echo '       "rootPatterns": [".flowconfig"],' >> "$dotfiles_folder"/coc-settings.json
-        echo '       "filetypes": ["javascript", "javascriptreact"]' >> "$dotfiles_folder"/coc-settings.json
-        echo -n '     }' >> "$dotfiles_folder"/coc-settings.json
-      fi
-    fi
-
-    echo '' >> "$dotfiles_folder"/coc-settings.json
-    echo '  }' >> "$dotfiles_folder"/coc-settings.json
-    echo '}' >> "$dotfiles_folder"/coc-settings.json
-
-    echo ' -> moving config file to .vim folder'
-    mv "$dotfiles_folder"/coc-settings.json ~/.vim/coc-settings.json
-    echo ' -> done'
+  echo '  -> running coc.nvim start.sh script'
+  cd ~/.janus/coc.nvim/
+  if [[ ! -x $(which yarn) ]]; then
+    npm install -g yarn
   fi
+  yarn install --frozen-lockfile
+  echo '  -> coc.nvim done'
+  if [[ ! -f "$dotfiles_folder"/coc-settings.json ]]; then
+    touch "$dotfiles_folder"/coc-settings.json
+  fi
+  echo '{' > "$dotfiles_folder"/coc-settings.json
+  echo '  "languageserver": {' >> "$dotfiles_folder"/coc-settings.json
+
+  # Gopls for COC
+  if [[ -x $(which go) ]] && [[ $(go version | grep 1.12) ]]; then
+    echo '  -> setting up gopls for coc'
+    go get -u golang.org/x/tools/cmd/gopls
+    if [[ -x $(which gopls) ]]; then
+      echo '  -> gopls installed'
+      echo '     "golang": {' >> "$dotfiles_folder"/coc-settings.json
+      echo '       "command": "gopls",' >> "$dotfiles_folder"/coc-settings.json
+      echo '       "args": [],' >> "$dotfiles_folder"/coc-settings.json
+      echo '       "rootPatterns": ["go.mod", ".vim/", ".git/", ".hg/"],' >> "$dotfiles_folder"/coc-settings.json
+      echo '       "filetypes": ["go"]' >> "$dotfiles_folder"/coc-settings.json
+      echo -n '     }' >> "$dotfiles_folder"/coc-settings.json
+    fi
+    rm -rf /tmp/tools
+    cd $godoc_files
+  fi
+
+  # Flow for COC (javascript completion shit)
+  if [[ -x $(which npm) ]]; then
+    echo ' -> setting up flow for coc'
+    npm install -g flow-bin
+    if [[ -x $(which flow)  ]]; then
+      echo ' -> flow installed'
+      echo ',' >> "$dotfiles_folder"/coc-settings.json
+      echo '     "flow": {' >> "$dotfiles_folder"/coc-settings.json
+      echo '       "command": "flow",' >> "$dotfiles_folder"/coc-settings.json
+      echo '       "args": ["lsp"],' >> "$dotfiles_folder"/coc-settings.json
+      echo '       "initializationOptions": {},' >> "$dotfiles_folder"/coc-settings.json
+      echo '       "settings": {},' >> "$dotfiles_folder"/coc-settings.json
+      echo '       "requireRootPattern": true,' >> "$dotfiles_folder"/coc-settings.json
+      echo '       "rootPatterns": [".flowconfig"],' >> "$dotfiles_folder"/coc-settings.json
+      echo '       "filetypes": ["javascript", "javascriptreact"]' >> "$dotfiles_folder"/coc-settings.json
+      echo -n '     }' >> "$dotfiles_folder"/coc-settings.json
+    fi
+  fi
+
+  echo '' >> "$dotfiles_folder"/coc-settings.json
+  echo '  }' >> "$dotfiles_folder"/coc-settings.json
+  echo '}' >> "$dotfiles_folder"/coc-settings.json
+
+  echo ' -> moving config file to .vim folder'
+  mv "$dotfiles_folder"/coc-settings.json ~/.vim/coc-settings.json
+  echo ' -> done'
 
   echo '-> Creating link for neovim'
   sudo chown -R $USER:$GROUP ~/.config
