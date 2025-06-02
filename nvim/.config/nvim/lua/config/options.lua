3f783da46dd65d43233b05de8f851bcb7875382c
Johmel Pintor <jahmgwapo@gmail.com>
2025-06-01 21:36:53 -0700

    Update options lua

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,70 +1,70 @@
 -- Assign set to vim.opt
-local set = vim.opt
+local set=vim.opt
 
 -- Mouse
-set.mouse = a
+set.mouse="a"
 
 -- Use OS clipboard
-set.clipboard = unnamed
+set.clipboard="unnamed"
 
 -- Set syntax
-set.syntax = on
+set.syntax="on"
 
 -- Don’t add empty newline at the end of files
-set.endofline = false
+set.endofline=false
 
 -- Use tabs rather than spaces
-set.expandtab = true
+set.expandtab=true
 
 -- 1 tab == 2 spaces
-set.shiftwidth = 2
-set.softtabstop = 2
-set.tabstop = 2
+set.shiftwidth=2
+set.softtabstop=2
+set.tabstop=2
 
 -- Linebreak on 500 characters
-set.linebreak = true
-set.textwidth = 500
-set.smartindent = true
+set.linebreak=true
+set.textwidth=500
+set.smartindent=true
 
 -- Respect modeline in files
-set.modelines = 4
+set.modelines=4
 
 -- Set relativenumber
-set.relativenumber = true
-set.number = true
+set.relativenumber=true
+set.number=true
 
 -- Highlight current line
-set.cursorline = true
+set.cursorline=true
 
 -- Ignore case of searches
-set.ignorecase = true
+set.ignorecase=true
 
 -- Don’t show the intro message when starting Vim
-set.shortmess = atI
+set.shortmess="atI"
 
 -- Show the filename in the window titlebar
-set.title = true
+set.title=true
 
 -- Start scrolling three lines before the horizontal window border
-set.scroll = 3
+set.scroll=3
 
 -- Folding settings
-set.foldmethod = indent
-set.foldnestmax = 10
-set.foldenable = false
-set.foldlevel = 1
+set.foldmethod="indent"
+set.foldnestmax=10
+set.foldenable=false
+set.foldlevel=1
 
 -- Better display for messages
-set.cmdheight = 2
+set.cmdheight=2
 
 -- Smaller updatetime for CursorHold & CursorHoldI
-set.updatetime = 300
+set.updatetime=300
 
 -- Always show signcolumns
-set.signcolumn = yes
+set.signcolumn="yes"
 
 -- Set filetype
-set.filetype = on
+set.filetype="on"
 
 -- All matches in a line are substituted
-set.gdefault = true
+set.gdefault=true
