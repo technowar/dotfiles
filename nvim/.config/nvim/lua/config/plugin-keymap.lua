6e6fb516ce5fd0456ab934209ad4d6662095d9a1
Johmel Pintor <jahmgwapo@gmail.com>
2025-07-17 21:06:41 -0700

    Update keymap for lsp

--- nvim/.config/nvim/lua/config/plugin-keymap.lua
+++ nvim/.config/nvim/lua/config/plugin-keymap.lua
@@ -17,11 +17,26 @@ vim.api.nvim_create_autocmd({ "LspAttach" }, {
 
 		local opts = { buffer = ev.buf }
 		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-		vim.keymap.set("n", "fd", vim.lsp.buf.type_definition, opts)
-		vim.keymap.set("n", "df", vim.diagnostic.open_float, opts)
 		vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+
+		vim.keymap.set("n", "gD", function()
+			vim.cmd("vsplit")
+			vim.lsp.buf.declaration()
+		end, opts)
+
+		vim.keymap.set("n", "gd", function()
+			vim.cmd("vsplit")
+			vim.lsp.buf.definition()
+		end, opts)
+
+		vim.keymap.set("n", "gi", function()
+			vim.cmd("vsplit")
+			vim.lsp.buf.implementation()
+		end, opts)
+
+		vim.keymap.set("n", "gf", function()
+			vim.cmd("vsplit")
+			vim.lsp.buf.type_definition()
+		end, opts)
 	end,
 })
