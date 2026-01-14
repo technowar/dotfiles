12d44a9e0eb307135fbbbe830529f85b206481f1
Johmel Pintor <jahm@Johmels-Air.setup>
2026-01-14 08:44:29 -0800

    Update start

--- start.sh
+++ start.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-function homebrew() {
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-}
-
 function tgpt() {
   curl -sSL https://raw.githubusercontent.com/aandrew-me/tgpt/main/install | /bin/bash -s ~/.bin/tgpt
 }
@@ -16,10 +12,23 @@ function z() {
   git clone https://github.com/rupa/z/ ~/.bin/z
 }
 
+function homebrew() {
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+}
+
 function setup() {
   if [ ! -x "$(command -v git)" ]; then
     echo Error: git is not installed
     exit 1
+  fi
+
+  # Install tgpt
+  if [ ! -d ~/.bin/tgpt ]; then
+    echo ' ------------- installing tgpt'
+    tgpt
+    echo ' ------------- tgpt installed'
+  else
+    echo ' ------------- tgpt is already installed'
   fi
 
   # Install tpm
@@ -48,21 +57,12 @@ function setup() {
   else
     echo ' ------------- Homebrew is already installed'
   fi
-
-  # Install tgpt
-  if [ ! -x "$(command -v tgpt)" ]; then
-    echo ' ------------- installing tgpt'
-    tgpt
-    echo ' ------------- tgpt installed'
-  else
-    echo ' ------------- tgpt is already installed'
-  fi
 }
 
 setup
 
+unset tgpt
 unset tpm
 unset z
 unset homebrew
-unset tgpt
 unset setup
